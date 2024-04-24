dc634f12bbb51e2442177a1114e075da853d14b8
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-04-24 12:30:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('Material Cycle Time Analysis')
 # File uploader
 uploaded_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
 if uploaded_file is not None:
-    data = pd.read_excel(uploaded_file)
+    data = pd.read_excel(uploaded_file, engine='openpyxl'))
     st.write(data)
 else:
     st.write("Please upload an Excel file to proceed.")
