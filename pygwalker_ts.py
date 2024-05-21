f53ad27713d2bbc899b4852991e0a474c46246b2
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-05-21 20:28:08 +0200

    Update pygwalker_ts.py

--- pygwalker_ts.py
+++ pygwalker_ts.py
@@ -7,10 +7,18 @@ st.set_page_config(
     page_title="Use Pygwalker In Streamlit",
     layout="wide"
 )
+#make this example reproducible
+np.random.seed(0)
+
+#create DataFrame with hourly index
+df = pd.DataFrame(index=pd.date_range('2020-01-06', '2020-12-27', freq='h'))
+
+#add column to show sales by hour
+df['sales'] = np.random.randint(low=0, high=20, size=len(df.index))
 # Import your data
-df = pd.read_excel("timeseries.xlsx")
-df_minute = pd.DataFrame()
-df_minute['RAW_VALUE'] = df['RAW_VALUE'].resample('D').sum()
-pyg_app = StreamlitRenderer(df_minute)
+# df = pd.read_excel("timeseries.xlsx")
+# df_minute = pd.DataFrame()
+#df_minute['RAW_VALUE'] = df['RAW_VALUE'].resample('D').sum()
+pyg_app = StreamlitRenderer(df)
  
 pyg_app.explorer()
