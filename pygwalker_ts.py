43e4014d2c504f1d24a61e76da6704bdf3306fe0
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-05-21 20:46:12 +0200

    Update pygwalker_ts.py

--- pygwalker_ts.py
+++ pygwalker_ts.py
@@ -12,17 +12,18 @@ st.set_page_config(
 np.random.seed(0)
 
 #create DataFrame with hourly index
-df = pd.DataFrame(index=pd.date_range('2020-01-06', '2020-12-27', freq='h'))
+# df = pd.DataFrame(index=pd.date_range('2020-01-06', '2020-12-27', freq='h'))
 
 #add column to show sales by hour
-df['sales'] = np.random.randint(low=0, high=20, size=len(df.index))
-weekly_df = pd.DataFrame()
-weekly_df['sales'] = df['sales'].resample('D').sum()
-weekly_df['TS'] = weekly_df.index
+# df['sales'] = np.random.randint(low=0, high=20, size=len(df.index))
+# weekly_df = pd.DataFrame()
+# weekly_df['sales'] = df['sales'].resample('D').sum()
+# weekly_df['TS'] = weekly_df.index
 # Import your data
-# df = pd.read_excel("timeseries.xlsx")
-# df_minute = pd.DataFrame()
-#df_minute['RAW_VALUE'] = df['RAW_VALUE'].resample('D').sum()
+df = pd.read_excel("timeseries.xlsx")
+df.set_index('TS', inplace=True)
+df_minute = pd.DataFrame()
+df_minute['RAW_VALUE'] = df['RAW_VALUE'].resample('D').sum()
 pyg_app = StreamlitRenderer(weekly_df)
  
 pyg_app.explorer()
