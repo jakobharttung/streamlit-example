4c9733e0026ae5d08047f4d3ada57f4d170dfb3d
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-05-23 09:25:01 +0200

    Update pyg_finance.py

--- pyg_finance.py
+++ pyg_finance.py
@@ -1,1 +1,37 @@
+import os
+import math
+import datetime
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
 
+def get_stock_data(ticker, years):
+    print(ticker)
+    end_date = datetime.now().date()
+    start_date = end_date - timedelta(days=years*365)
+    print(start_date)
+    stock = yf.Ticker(ticker)
+
+    # Retrieve historical price data
+    hist_data = stock.history(start=start_date, end=end_date)
+
+    # Retrieve balance sheet
+    balance_sheet = stock.balance_sheet
+
+    # Retrieve financial statements
+    financials = stock.financials
+
+    # Retrieve financial info
+    info = stock.info
+
+    # Retrieve news articles
+    news = stock.news
+
+    return hist_data, balance_sheet, financials, info, news
+
+tickers = np.array['MSFT, 'NVDA', 'ANET', 'TSLA']
+years =  3
+hist_data, balance_sheet, financials, info, news = get_stock_data(ticker, years)
